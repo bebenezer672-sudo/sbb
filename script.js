3cb515d4c18523beadeb4d98bf5b1c8bea449f98
Ebenezer Boateng <e.boateng@s-bb.nl>
2025-10-03 11:54:10 +0200

    Add contact page with map and remove contact popup from main page

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const ele = document.createElement()
+const openBtn = document.querySelector("#openBtn");
+const popup = document.querySelector("#popup");
+const closeBtn = document.querySelector("#closeBtn");
+
+
+
+
+openBtn.addEventListener("click", () => {
+  popup.style.display = "flex";
+})
+
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+})
+
+
+
