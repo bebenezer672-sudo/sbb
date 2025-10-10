5bcac334865b6f0ac2f0ba8c80901289d5a8abb9
Ebenezer Boateng <e.boateng@s-bb.nl>
2025-10-10 12:02:47 +0200

    Add cart system with HTML and CSS

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,30 @@
 const buttons = document.querySelectorAll(".add-to-cart");
 const prices = document.querySelectorAll(".price");
-
+let cart = document.querySelector(".cart-count");
+let cartcount = 0;
 buttons.forEach((button, index) => {
   const priceElement = prices[index];
   const priceText = priceElement.textContent;
 
   button.addEventListener("click", () => {
+    cartcount++;
     alert(priceText + " Add to the cart");
+    
+   if(cart){
+    cart.textContent = cartcount
+   }
+
   });
 });
+
+
+
+const cartBtn = document.querySelector(".cartBtn");
+
+cartBtn.addEventListener("click", () => {
+  
+});
+
 
 
 
